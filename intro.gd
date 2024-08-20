cc3de1cf77954790252bfa66e16bd2c5bd82b84a
Celia <celiavillagj@gmail.com>
2024-08-20 11:31:20 +0200

    crecimiento

--- intro.gd
+++ intro.gd
@@ -3,10 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("fade in")
-	await(get_tree().create_timer(10)).timeout
+	#$AnimationPlayer.play("fade in")
+	#await(get_tree().create_timer(10)).timeout
 	get_tree().change_scene_to_file("res://scenes/tree_level.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
